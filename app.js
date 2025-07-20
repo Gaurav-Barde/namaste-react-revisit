2ff18eac2c2713ec3009a9f2c0e4b758994c56a0
Gaurav-Barde <gabarde25@gmail.com>
2025-07-20 19:04:44 +0530

    corrected sentences

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "title" }, "I am H1 tag"),
-    React.createElement("h2", { id: "subTitle" }, "I am H2 tag"),
+    React.createElement("h1", { id: "title" }, "I am an H1 tag"),
+    React.createElement("h2", { id: "subTitle" }, "I am an H2 tag"),
   ])
 );
 
