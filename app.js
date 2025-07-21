9d9fb543f8f5e184ae35214b4b15afe628031fff
Gaurav-Barde <gabarde25@gmail.com>
2025-07-21 23:48:17 +0530

    installed react and react-dom using npm and removed cdn links

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "title" }, "I am an H1 tag"),
-    React.createElement("h2", { id: "subTitle" }, "I am an H2 tag"),
+    React.createElement("h1", { id: "title", key: "1" }, "I am an H1 tag"),
+    React.createElement("h2", { id: "subTitle", key: "2" }, "I am an H2 tag"),
   ])
 );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(parent);
