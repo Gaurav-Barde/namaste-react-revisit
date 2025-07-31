3f70709df49bd1f3060fa25265d6623a2a3134f9
Gaurav-Barde <gabarde25@gmail.com>
2025-08-01 00:55:15 +0530

    created routing configurations and added sample pages

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+const Contact = () => {
+  return (
+    <div className="contact-container">
+      <h1>Contact Us:</h1>
+      <h3>You can contact us at following number</h3>
+      <h4>+91 8727272822</h4>
+    </div>
+  );
+};
+
+export default Contact;
