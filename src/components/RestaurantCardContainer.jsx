f974798fc4a07a64c060b7134ca979447e56d666
Gaurav-Barde <gabarde25@gmail.com>
2025-07-31 16:40:04 +0530

    added filters

--- src/components/RestaurantCardContainer.jsx
+++ src/components/RestaurantCardContainer.jsx
@@ -1,18 +1,16 @@
 import { useEffect, useState } from "react";
 import { ResCard } from "./ResCard";
+import Search from "./Search";
 import { RES_LIST_API_URL } from "../utils/constants";
+import Shimmer from "./Shimmer";
 
 const RestaurantCardContainer = () => {
   const [resList, setResList] = useState([]);
+  const [filteredResList, setFilteredResList] = useState([]);
 
   useEffect(() => {
     fetchData();
   }, []);
-
-  const filterButtonHandler = () => {
-    const filteredData = resData.filter((res) => res.info.avgRating > 4);
-    setResList(filteredData);
-  };
 
   const fetchData = async () => {
     const data = await fetch(RES_LIST_API_URL);
@@ -21,15 +19,38 @@ const RestaurantCardContainer = () => {
       json?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants
     );
+    setFilteredResList(
+      json?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
   };
+
+  const filterButtonHandler = () => {
+    const filteredData = resList.filter((res) => res.info.avgRating > 4.5);
+    setResList(filteredData);
+  };
+
+  const searchFilteredResList = (searchText) => {
+    const filteredResList = resList.filter((res) => {
+      return res.info.name.toLowerCase().includes(searchText.toLowerCase());
+    });
+    setFilteredResList(filteredResList);
+  };
+
+  if (resList.length === 0) {
+    return <Shimmer />;
+  }
 
   return (
     <div>
-      <button className="filter-button" onClick={filterButtonHandler}>
-        Top Rated Restaurants
-      </button>
+      <div className="res-container-header">
+        <Search searchFilteredResList={searchFilteredResList} />
+        <button className="button" onClick={filterButtonHandler}>
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {filteredResList.map((restaurant) => (
           <ResCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,30 @@
-const Search = () => {
-  return <div className="search-container">Search</div>;
+import { useState } from "react";
+
+const Search = ({ searchFilteredResList }) => {
+  const [searchText, setSearchText] = useState("");
+
+  const onChangeHandler = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  const searchButtonHandler = () => {
+    searchFilteredResList?.(searchText);
+  };
+
+  return (
+    <div className="search-container">
+      <input
+        className="search"
+        type="text"
+        placeholder="Search Restaurants"
+        value={searchText}
+        onChange={onChangeHandler}
+      />
+      <button className="button" onClick={searchButtonHandler}>
+        Search
+      </button>
+    </div>
+  );
 };
 
 export default Search;
