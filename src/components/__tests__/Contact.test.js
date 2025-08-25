7d73cd1410569aabf0fcd0385d058cfe3ee2049a
Gaurav-Barde <gabarde25@gmail.com>
2025-08-26 01:21:18 +0530

    added tests for Header component

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -2,8 +2,22 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Contact from "../Contact";
 
-test("Should load Contact component", () => {
-  render(<Contact />);
-  const renderedComp = screen.getByRole("heading");
-  expect(renderedComp).toBeInTheDocument();
+describe("Contact component test cases", () => {
+  test("Should load Contact component", () => {
+    render(<Contact />);
+    const renderedComp = screen.getByRole("heading");
+    expect(renderedComp).toBeInTheDocument();
+  });
+
+  test("Should load button inside Contact component", () => {
+    render(<Contact />);
+    const button = screen.getByText("Submit");
+    expect(button).toBeInTheDocument();
+  });
+
+  test("Should load 3 input inside Contact component", () => {
+    render(<Contact />);
+    const emailInput = screen.getAllByRole("textbox");
+    expect(emailInput.length).toBe(3);
+  });
 });
